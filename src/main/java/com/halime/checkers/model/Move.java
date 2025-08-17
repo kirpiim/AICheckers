63584a157b75fd91febea7cdd62d1f3e68fab093
Your Name <your@email.com>
2025-08-17 17:44:06 +0800

    added Move class to represent piece moves
    - Created Move class with startRow, startCol, endRow, endCol
    - Added support for storing captured pieces in a move
    - Provides helper methods for adding and retrieving captured pieces

---
+++ src/main/java/com/halime/checkers/model/Move.java
@@ -0,0 +1,44 @@
+package com.halime.checkers.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Move {
+    private int startRow;
+    private int startCol;
+    private int endRow;
+    private int endCol;
+    private List<Piece> capturedPieces;
+
+    public Move(int startRow, int startCol, int endRow, int endCol) {
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.endRow = endRow;
+        this.endCol = endCol;
+        this.capturedPieces = new ArrayList<>();
+    }
+
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public int getStartCol() {
+        return startCol;
+    }
+
+    public int getEndRow() {
+        return endRow;
+    }
+
+    public int getEndCol() {
+        return endCol;
+    }
+
+    public List<Piece> getCapturedPieces() {
+        return capturedPieces;
+    }
+
+    public void addCapturedPiece(Piece piece) {
+        capturedPieces.add(piece);
+    }
+}
